e40aa306f142b25af997f5c6c806c17a2f9ec445
winniecl <chongluiwinnie@gmail.com>
2022-09-10 14:41:41 +0800

    ignore

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require("express");
+const path = require("path");
 var app = express();
 const PORT = process.env.PORT || 5050;
 app.get("/", (req, res) => {
@@ -10,54 +11,55 @@ app.listen(PORT, function () {
 
 const { categories } = require("./handlers/categories");
 const { db, admin } = require("./utils/admin");
-//const { firebase } = require("./utils/firebase");
 app.get("/categories", categories);
-const catData = {
+const categoryData = {
   name: "Hello",
-  parentPromotionCategoryId: "1",
+  parentpromotionDataCategoryId: "1",
   subCategoryCount: 0,
 };
-const addParent = async (catName, catData) => {
-  const parentCat = db.collection(catName);
-  const res = await parentCat.add(catData);
+const addCategory = async (pathParam, categoryData) => {
+  const parentCat = db.doc(pathParam);
+
+  const subCategory = await category.collection(promotionData.name).get();
+  if (categoryData.subCategoryCount === 0 && subCategory.empty) {
+    console.log("yes");
+    parentCat.update({
+      subCategoryCount: admin.firestore.FieldValue.increment(1),
+    });
+  }
+
+  const res = await parentCat.add(categoryData);
   parentCat.doc(res.id).update({
     id: res.id,
   });
   return res.id;
 };
+//addCategory("categories/42BqeaKmGVLZmK79usOd/Top Cosmetics", categoryData);
+const pathParam = "categories/42BqeaKmGVLZmK79usOd/Top Cosmetics";
+console.log(pathParam.split(path.sep).length);
 
-const promotion = {
+const promotionData = {
   name: "PromoCat1",
   description: "<div>Hello<br/>This is nice!</div>",
   imageUrlList: ["http://localhost:3000/assets/123.jpg"],
-  subCategoryCount: 0,
 };
-const addAPromotion = async (path, promotion) => {
-  const category = db.doc(path);
-  const subCategory = await category.collection(promotion.name).get();
-  if (promotion.subCategoryCount === 0 && subCategory.empty) {
-    console.log("yes");
-    category.update({
-      subCategoryCount: admin.firestore.FieldValue.increment(1),
-    });
-  } else console.log("no");
+const addPromotionData = async (pathParam, promotionData) => {
+  const category = db.doc(pathParam);
+
   const catID = await category
     .get()
-    .then((doc) => doc.data().parentPromotionCategoryId);
+    .then((doc) => doc.data().parentpromotionDataCategoryId);
   category
-    .collection(promotion.name)
-    .add(promotion)
+    .collection(promotionData.name)
+    .add(promotionData)
     .then((res) => {
-      category.collection(promotion.name).doc(res.id).update({
+      category.collection(promotionData.name).doc(res.id).update({
         id: res.id,
-        promotionCategoryId: catID,
+        promotionDataCategoryId: catID,
       });
     });
 };
-// addParent("Editor’s Favorite Housewares", catData).then((id) => {
-//   addAPromotion("Editor’s Favorite Housewares/" + id, promotion);
+// addCategory("Editor’s Favorite Housewares", categoryData).then((id) => {
+//   addPromotionData("Editor’s Favorite Housewares/" + id, promotionData);
 // });
-addAPromotion(
-  "Editor’s Favorite Housewares/" + "WdKlnHMo7GQ6xrSijTD4",
-  promotion
-);
+//addPromotionData("categories/Top Cosmetics", promotionData);
